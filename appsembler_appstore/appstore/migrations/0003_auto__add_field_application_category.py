36f2789e10f2ad4a85feb60e50a81625422de604
Filip Jukic <filip@jukic.me>
2013-07-12 11:42:16 -0400

    added relationship between applications and categories

---
+++ appsembler_appstore/appstore/migrations/0003_auto__add_field_application_category.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Application.category'
+        db.add_column(u'appstore_application', 'category',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=0, related_name='applications', to=orm['appstore.AppCategory']),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Application.category'
+        db.delete_column(u'appstore_application', 'category_id')
+
+
+    models = {
+        u'appstore.appcategory': {
+            'Meta': {'object_name': 'AppCategory'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': u"orm['appstore.AppCategory']"}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        u'appstore.application': {
+            'Meta': {'object_name': 'Application'},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'applications'", 'to': u"orm['appstore.AppCategory']"}),
+            'demo_site_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'license_type': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'source_code_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['appstore']
--- appsembler_appstore/appstore/models.py
+++ appsembler_appstore/appstore/models.py
@@ -5,6 +5,7 @@ from categories.models import CategoryBase
 class Application(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    category = models.ForeignKey("AppCategory", related_name="applications")
     logo = models.ImageField(upload_to='app_logos')
     author = models.CharField(max_length=100, blank=True)
     version = models.CharField(max_length=15, blank=True)
