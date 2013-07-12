167f2ea976ba428be4df168579813403c70190be
Filip Jukic <filip@jukic.me>
2013-07-12 15:00:35 -0400

    register Application model in the admin

--- appsembler_appstore/appstore/admin.py
+++ appsembler_appstore/appstore/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from categories.admin import CategoryBaseAdmin
-from .models import AppCategory
+from .models import AppCategory, Application
 
 
 class AppCategoryAdmin(CategoryBaseAdmin):
     pass
 
 
+admin.site.register(Application)
 admin.site.register(AppCategory, AppCategoryAdmin)
