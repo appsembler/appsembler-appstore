b1e4f94424d0704eca8fb09d519ce49de491300d
Filip Jukic <filip@jukic.me>
2013-07-12 16:55:02 -0400

    added media files serving in development

--- appsembler_appstore/appsembler_appstore/urls.py
+++ appsembler_appstore/appsembler_appstore/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 
@@ -8,4 +10,4 @@ urlpatterns = patterns('',
     url(r'^appstore/', include('appstore.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
