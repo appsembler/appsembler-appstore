597c386db2ba4c7d0a0357c313d95f8f07e46abb
Filip Jukic <filip@jukic.me>
2013-07-12 17:06:41 -0400

    refactored templates a bit, added app category default view

--- appsembler_appstore/appstore/urls.py
+++ appsembler_appstore/appstore/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from .views import AppCategoryDetailView
 
 urlpatterns = patterns('',
+    url(r'^(?P<slug>\w+)/$', AppCategoryDetailView.as_view(), name='appstore_appcategory_detail'),
     url(r'^$', TemplateView.as_view(template_name='appstore/index.html'), name='appstore_index'),
 )
--- appsembler_appstore/appstore/views.py
+++ appsembler_appstore/appstore/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.views.generic import DetailView
+from .models import AppCategory
+
+
+class AppCategoryDetailView(DetailView):
+    queryset = AppCategory.objects.all()
+    context_object_name = "appcategory"
