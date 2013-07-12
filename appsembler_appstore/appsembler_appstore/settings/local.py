d6602414571d98166875ccb457dfc89eae5160b4
Filip Jukic <filip@jukic.me>
2013-07-12 16:55:15 -0400

    added django debug toolbar for development

--- appsembler_appstore/appsembler_appstore/settings/local.py
+++ appsembler_appstore/appsembler_appstore/settings/local.py
@@ -1,1 +1,20 @@
 from .base import *
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
